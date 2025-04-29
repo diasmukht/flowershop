711195a3638e99fcd6d9cc0d77bbd3d0e5a73507
diasmukht <diaspias05@gmail.com>
2025-04-29 19:30:27 +0600

    удалён временный маршрут /run-migrations/

--- FlowersShop/urls.py
+++ FlowersShop/urls.py
@@ -7,13 +7,7 @@ from django.conf.urls.static import static
 from django.core.management import call_command
 from django.http import HttpResponse
 
-def run_migrations(request):
-    try:
-        call_command('migrate')
-        return HttpResponse("✅ Миграции успешно выполнены!")
-    except Exception as e:
-        import traceback
-        return HttpResponse(f"❌ Ошибка миграции: {e}<br><pre>{traceback.format_exc()}</pre>")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -21,7 +15,7 @@ urlpatterns = [
     path('', include('main.urls')),           # Все остальные страницы из приложения main
     path('users/', include('users.urls')),    # Аутентификация пользователей
     path('constructor/', include(('constructor.urls', 'constructor'), namespace='constructor')),  # Конструктор букетов
-    path('run-migrations/', run_migrations),
+
 ]
 
 # Подключаем доступ к медиа-файлам (для картинок цветов, упаковки и пр.)
