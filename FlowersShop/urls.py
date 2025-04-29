705a0fb618ee2015da6bc32a444bca9e97017ed5
diasmukht <diaspias05@gmail.com>
2025-04-29 20:13:08 +0600

    chore: удалён временный маршрут /migrate-now/

--- FlowersShop/urls.py
+++ FlowersShop/urls.py
@@ -4,23 +4,16 @@ from django.urls import path, include
 from main import views as main_views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.core.management import call_command
-from django.http import HttpResponse
-import traceback
 
-def run_migrations(request):
-    try:
-        call_command('migrate')
-        return HttpResponse("✅ Миграции успешно применены!")
-    except Exception as e:
-        return HttpResponse(f"❌ Ошибка миграции: {e}<br><pre>{traceback.format_exc()}</pre>")
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('users/', include('users.urls')),    # Аутентификация пользователей
     path('constructor/', include(('constructor.urls', 'constructor'), namespace='constructor')),  # Конструктор букетов
-    path('migrate-now/', run_migrations),  # Временный маршрут для запуска миграций
+   
 ]
 
 # Подключаем доступ к медиа-файлам (для картинок цветов, упаковки и пр.)
